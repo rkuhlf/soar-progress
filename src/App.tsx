a74a7013b91ab404ad136638e5063b64980528d0
Riley Kuhlman <rileyk2@icloud.com>
2023-09-21 19:30:40 -0500

    Clean up stuff so it builds

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,6 @@ import LoginPage from './components/LoginPage';
 import { useLoginContext } from './components/LoginProvider';
 
 
-function testEndpoint() {
-  fetch('/.netlify/functions/hello')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Response:', data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-
-}
-
 function App() {
   const { profile } = useLoginContext();
   
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useLoginContext } from "./LoginProvider";
 import type { TaskData } from "./Task";
 import Task from "./Task";
