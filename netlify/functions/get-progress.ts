d00bd71f4f8d1a77a1c8bce0f7afb4440fd74a83
Riley Kuhlman <rileyk2@icloud.com>
2025-09-18 17:13:23 -0500

    Change to the new google sheet

--- netlify/functions/get-progress.ts
+++ netlify/functions/get-progress.ts
@@ -8,10 +8,10 @@ import { start } from "repl";
 
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-const SHEET_ID = "1f5O1H-XR6goR3GSy-CYepBoQpsup4o8skqChsYR0V3E";
+const SHEET_ID = "1wmBYYc2s6NFeIJZIScO6k_6X0W3LMSseECwwpKHNZ2w";
 const GOOGLE_SHEET_KEY = process.env.GOOGLE_SHEET_KEY?.split(String.raw`\n`).join('\n');
 // In addition to updating this, you have to update all of the indices where the columns are, e.g. startColumnLookup.
-const IS_SPRING = true;
+const IS_SPRING = false;
 
 if (!GOOGLE_SHEET_KEY) {
   throw Error("Could not determine google sheet key.");
@@ -79,57 +79,58 @@ function nameMatches(targetName: string, firstName: string, lastName: string): b
 }
 
 const startColumnLookup = {
-  "Pilot Your Potential": 27,
-  "Elevate Your Expectations": 28,
-  "Look To Launch": 26,
-  "Take Flight": 27,
-  "IYA1": 26,
-  "IYA2": 26,
+  "Pilot Your Potential": 5,
+  "Elevate Your Expectations": 5,
+  "Look To Launch": 5,
+  "Take Flight": 5, // f
+  "IYA1": 5,
+  "IYA2": 5,
 }
 
-// startColumn lookup for first semester.
-// "Pilot Your Potential": 10,
-// "Elevate Your Expectations": 10,
-// "Look To Launch": 10,
-// "Take Flight": 11,
-// "IYA1": 11,
-// "IYA2": 10,
+// startColumn lookup for second semester.
+// "Pilot Your Potential": 27,
+//   "Elevate Your Expectations": 28,
+//   "Look To Launch": 26,
+//   "Take Flight": 27,
+//   "IYA1": 26,
+//   "IYA2": 26,
 
 
 // Currently set to skip the ACH stuff.
 // End column should be inclusive.
 const endColumnLookup = {
-  "Pilot Your Potential": 34,
-  "Elevate Your Expectations": 35,
-  "Look To Launch": 34,
-  "Take Flight": 35,
-  "IYA1": 32,
-  "IYA2": 32,
+  "Pilot Your Potential": 13,
+  "Elevate Your Expectations": 14,
+  "Look To Launch": 12,
+  "Take Flight": 12, // m
+  "IYA1": 11,
+  "IYA2": 11,
 }
 
-// First semester numbers
-// "Pilot Your Potential": 17,
-// "Elevate Your Expectations": 18,
-// "Look To Launch": 16,
-// "Take Flight": 17,
-// "IYA1": 16,
-// "IYA2": 16,
+// Second semester numbers
+// "Pilot Your Potential": 34,
+// "Elevate Your Expectations": 35,
+// "Look To Launch": 34,
+// "Take Flight": 35,
+// "IYA1": 32,
+// "IYA2": 32,
+
 
 const nameLookup = {
-  "Pilot Your Potential": 22,
-  "Elevate Your Expectations": 23,
-  "Look To Launch": 21,
-  "Take Flight": 22,
-  "IYA1": 21,
-  "IYA2": 21,
+  "Pilot Your Potential": 1,
+  "Elevate Your Expectations": 1,
+  "Look To Launch": 1,
+  "Take Flight": 1,
+  "IYA1": 1,
+  "IYA2": 1,
 }
 
-// "Pilot Your Potential": 1,
-// "Elevate Your Expectations": 1,
-// "Look To Launch": 1,
-// "Take Flight": 1,
-// "IYA1": 1,
-// "IYA2": 1,
+// "Pilot Your Potential": 22,
+// "Elevate Your Expectations": 23,
+// "Look To Launch": 21,
+// "Take Flight": 22,
+// "IYA1": 21,
+// "IYA2": 21,
 
 const headerRowLookup = {
   "Pilot Your Potential": 2,
